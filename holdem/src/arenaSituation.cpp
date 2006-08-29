4e5699c920dac83a7d25764b3a4afeb6bfa84a4c
Joseph Huang <yuzisee@gmail.com>
2006-08-29 00:23:27 +0000

    Iterative ExactCall algorithm implemented

---
+++ holdem/src/arenaSituation.cpp
@@ -0,0 +1,133 @@
+/***************************************************************************
+ *   Copyright (C) 2006 by Joseph Huang                                    *
+ *                                                                         *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include "arenaSituation.h"
+
+
+ExpectedCallD::~ExpectedCallD()
+{
+}
+
+float64 ExpectedCallD::potFraction() const
+{
+    return (  table->GetPotSize() / table->ViewPlayer(playerID)->GetMoney()  );
+}
+
+
+float64 ExpectedCallD::betFraction(const float64 betSize) const
+{
+    return (  betSize / table->ViewPlayer(playerID)->GetMoney()  );
+}
+/*
+float64 EstimateCallD::exf(float64 betSize) const
+{
+    const float64& x = betFraction(betSize);
+    return table->GetNumberInHand() * e->pctWillCall(  x/(2*x+potFraction() )  );
+}
+
+
+float64 EstimateCallD::dexf(float64 betSize) const
+{
+    const float64& f_pot = potFraction();
+    const float64& x = betFraction(betSize);
+    return table->GetNumberInHand() * e->pctWillCallD(  x/(2*x+f_pot)  ) * f_pot / (2*x+f_pot) /(2*x+f_pot);
+}
+*/
+
+
+void ExactCallD::query(float64 betSize)
+{
+
+
+    totalexf = table->GetPotSize() + betSize;
+    //float64 lastexf = totalexf;
+
+    totaldexf = 0;
+    //float64 lastdexf = totaldexf;
+
+
+
+    int8 pIndex = playerID;
+
+    do{
+        table->incrIndex(pIndex);
+        if( table->CanStillBet(pIndex) )
+        {///Predict how much the bet will be
+            float64 oppBankRoll = table->ViewPlayer(pIndex)->GetMoney();
+            float64 nextexf;
+            float64 nextdexf;
+
+            if( betSize >= oppBankRoll )
+            {
+                float64 oppBetMake = betSize - table->ViewPlayer(pIndex)->GetBetSize();
+                //To understand the above, consider that totalexf ALREADY includes
+
+                nextexf = oppBankRoll * e->pctWillCall(oppBetMake / (oppBetMake + totalexf) );
+                nextdexf = oppBankRoll * e->pctWillCallD(  oppBetMake/(oppBetMake+totalexf)  )
+                                                * (totalexf - oppBetMake * totaldexf)
+                                                 /(oppBetMake + totalexf) /(oppBetMake + totalexf);
+            }else
+            {///Opponent would be all-in to call this bet
+                float64 deadpot = table->GetDeadPotSize();
+                float64 effroundpot = (totalexf - deadpot) * oppBankRoll / betSize;
+                float64 oppBetMake = oppBankRoll - table->ViewPlayer(pIndex)->GetBetSize();
+                nextexf = oppBankRoll * e->pctWillCall(oppBetMake / (oppBetMake + deadpot + effroundpot) );
+                nextdexf = 0;
+            }
+            //lastexf = nextexf;
+            totalexf += nextexf;
+
+            //lastdexf = nextdexf;
+            totaldexf += nextdexf;
+        }
+    }while( pIndex != playerID );
+
+    totalexf = betFraction(totalexf);
+    totaldexf = betFraction(totaldexf);
+}
+
+float64 ExactCallD::exf(float64 betSize)
+{
+    //if( queryinput == UNINITIALIZED_QUERY )
+    if( queryinput != betSize )
+    {
+        query(betSize);
+        queryinput = betSize;
+    }
+    return totalexf;
+}
+
+float64 ExactCallD::dexf(float64 betSize)
+{
+//    if( query == UNINITIALIZED_QUERY )
+    if( queryinput != betSize )
+    {
+        query(betSize);
+        queryinput = betSize;
+    }
+    return totaldexf;
+}
+
+
+
+
+
+
+
---
+++ holdem/src/arenaSituation.h
@@ -0,0 +1,108 @@
+/***************************************************************************
+ *   Copyright (C) 2006 by Joseph Huang                                    *
+ *                                                                         *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifndef HOLDEM_ArenaSituations
+#define HOLDEM_ArenaSituations
+
+#include "arena.h"
+/*
+class ExpectedCall
+{
+    protected:
+
+    const HoldemArena* table;
+    const int8 playerID;
+    public:
+    virtual float64 exf(float64 betSize);
+}
+;
+*/
+class ExpectedCallD /*: public virtual ExpectedCall*/
+{
+protected:
+    const int8 playerID;
+    const HoldemArena* table;
+    const CallCumulationD* e;
+public:
+    ExpectedCallD(const int8 id, const HoldemArena* base, const CallCumulationD* data)
+    : playerID(id), table(base), e(data)
+    {}
+
+    virtual ~ExpectedCallD();
+
+    virtual float64 potFraction() const;
+    virtual float64 betFraction(const float64 betSize) const;
+    virtual float64 exf(float64 betSize) = 0;
+    virtual float64 dexf(float64 betSize) = 0;
+}
+;
+
+/*
+class EstimateCallD : public virtual ExpectedCallD
+{
+public:
+
+    EstimateCallD(const int8 id, const HoldemArena* base, const CallCumulationD* data)
+    : ExpectedCallD(id,base,data)
+    {};
+
+    virtual float64 exf(float64 betSize);
+    virtual float64 dexf(float64 betSize);
+}
+;
+*/
+
+class ExactCallD : public virtual ExpectedCallD
+{
+protected:
+    static const float64 UNITIALIZED_QUERY = -1;
+    float64 queryinput;
+    float64 totalexf;
+    float64 totaldexf;
+
+    void query(float64 betSize);
+public:
+    ExactCallD(const int8 id, const HoldemArena* base, const CallCumulationD* data)
+    : ExpectedCallD(id,base,data)
+    {
+        queryinput = UNITIALIZED_QUERY;
+    };
+
+    virtual float64 exf(float64 betSize);
+    virtual float64 dexf(float64 betSize);
+}
+;
+
+/*
+class DummyArena : public virtual HoldemArena
+{
+public:
+
+    DummyArena(BlindStructure* b, bool illustrate) : HoldemArena(b,illustrate)
+    {}
+
+    virtual const Player* ViewPlayer(int8) const;
+}
+;
+*/
+
+
+#endif
+
