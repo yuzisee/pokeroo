4dac72ea5592e0b9e44971b4bdd73b6a462febab
Joseph Huang <yuzisee@gmail.com>
2008-03-23 07:12:46 +0000

    Typo in false position

--- holdem/src/functionbase.cpp
+++ holdem/src/functionbase.cpp
@@ -566,7 +566,7 @@ float64 ScalarFunctionModel::bisectionStep(float64 x1, float64 x2) const
 
 float64 ScalarFunctionModel::regularfalsiStep(float64 x1, float64 y1, float64 x2, float64 y2) const
 {//A single step from the method of False Positions
-	return (x1*y2 + x2*y1)/(y2 - y1);
+	return (x1*y2 - x2*y1)/(y2 - y1);
 }
 
 float64 ScalarFunctionModel::FindZero(float64 x1, float64 x2)
