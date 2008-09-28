3ac60a52c1df37eb6c77a70f8bbd6f7b33d2a018
Joseph Huang <yuzisee@gmail.com>
2008-09-28 17:11:53 +0000

    We will use Python->C->C++ in three steps

--- holdem/src/interfacePython.cpp
+++ holdem/src/interfacePython.cpp
@@ -18,15 +18,10 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
-#include "Python.h"
+#include "portability.h"
+#include <string>
 
-typedef double float64;
-typedef unsigned char int8;
-
-//#include "portability.h"
-//#include <string.h>
-
-//using std::string;
+using std::string;
 
 
 /*****************************************************************************
@@ -36,52 +31,36 @@ typedef unsigned char int8;
 
 
 ///Get the amount of money playerNumber has in front of him
-static PyObject * GetMoney (PyObject *self, PyObject *args) //float64 (int8 playerNumber);
-{
-    //TODO: If the player is all in, make sure this returns the correct value
-}
+extern "C" float64 GetMoney(int8 playerNumber);
+//TODO: If the player is all in, make sure this returns the correct value
+
 
 
 //Override the amount of money playerNumber has in front of him
-static PyObject * SetMoney (PyObject *self, PyObject *args) ////void (int8 playerNumber, float64 money);
-{
-}
+//void SetMoney(int8 playerNumber, float64 money);
 
 
 
 ///Get the amount of money playerNumber has bet so far this round
-static PyObject * GetBet (PyObject *self, PyObject *args) //float64 (int8 playerNumber);
-{
-    //TODO: If the player is all in, make sure this returns the correct value
-}
-
+extern "C" float64 GetBet(int8 playerNumber);
+///TODO: If the player is all in, make sure this returns the correct value
 
 
 
 ///Get the amount of money that is in the pot
-static PyObject * GetPotSize (PyObject *self, PyObject *args) //float64 ();
-{
-}
+extern "C" float64 GetPotSize();
 
 
 ///Get the amount of money that was in the pot at the BEGINNING of the current betting round
-static PyObject * GetLastRoundPotsize (PyObject *self, PyObject *args) //float64 ();
-{
-}
+extern "C" float64 GetLastRoundPotsize();
 
 
 ///Get the size of the highest bet so far
-static PyObject * GetBetToCall (PyObject *self, PyObject *args) //float64 ();
-{
-}
-
+extern "C" float64 GetBetToCall();
 
 
 //Get the playerNumber of the player who's turn it is
-static PyObject * WhoIsNext (PyObject *self, PyObject *args) //int8 ();
-{
-}
-
+extern "C" int8 WhoIsNext();
 
 /*****************************************************************************
 	Betting round accessors
@@ -102,10 +81,7 @@ need to use any of the Start______() functions.
 int8 bAutoMode = 0;
 
 ///Use this to start the game and set bAutoMode = 1; See the note above.
-static PyObject * AutoPlayGame (PyObject *self, PyObject *args) //void ();
-{
-}
-
+extern "C" void AutoPlayGame();
 
 
 ///Call NewCommunityCard for each card that is dealt to the table during flop/turn/river
@@ -134,72 +110,51 @@ cardSuit can be any of:
 'D' for Diamonds
 */
 ///For example, for the eight of hearts: cardValue = '8' and cardSuit = 'H'
-static PyObject * NewCommunityCard (PyObject *self, PyObject *args) //void (char cardValue,char cardSuit);
-{
-}
+extern "C" void NewCommunityCard(char cardValue,char cardSuit);
 
 
 
 
 
 ///Call this when the betting begins
-static PyObject * StartBetting (PyObject *self, PyObject *args) //void ();
-{
-}
-
+extern "C" void StartBetting();
 
 ///Call these functions when playerNumber Raises, Folds, or Calls
-static PyObject * PlayerCalls (PyObject *self, PyObject *args) //void (int8 playerNumber);
-{}
-
-static PyObject * PlayerFolds (PyObject *self, PyObject *args) //void (int8 playerNumber);
-{}
-
-static PyObject * PlayerRaisesTo (PyObject *self, PyObject *args) //void (int8 playerNumber, float64 amount);
-{}
-
-static PyObject * PlayerRaisesBy (PyObject *self, PyObject *args) //void (int8 playerNumber, float64 amount);
-{}
+extern "C" void PlayerCalls(int8 playerNumber);
+extern "C" void PlayerFolds(int8 playerNumber);
+extern "C" void PlayerRaisesTo(int8 playerNumber, float64 amount);
+extern "C" void PlayerRaisesBy(int8 playerNumber, float64 amount);
 ///Question: If a player doesn't call any of these, which is the default action?
 
 
-
 ///GetBetAmount is useful for asking a bot what to bet
-static PyObject * GetBetAmount (PyObject *self, PyObject *args) //float64 (int8 playerNumber);
-{
-}
+extern "C" float64 GetBetAmount(int8 playerNumber);
 
 ///GetAction returns a string descfribing what playerNumber wants to do in this situation
 ///This is particularly useful for bots
-static PyObject * GetAction (PyObject *self, PyObject *args) //string (int8 playerNumber);
-{
-}
+extern "C" string GetAction(int8 playerNumber);
+
+
 
 
 
 ///Call this when (if) the showdown begins
-static PyObject * StartShowdown (PyObject *self, PyObject *args) //void ();
-{
-}
+extern "C" void StartShowdown();
 
 ///Call this for each card playerNumber reveals during the showdown
 ///See NewCommunityCard for usage of cardValue and cardSuit
-static PyObject * PlayerShowsCard (PyObject *self, PyObject *args) //void (int8 playerNumber, char cardValue, char cardSuit);
-{
-}
+extern "C" void PlayerShowsCard(int8 playerNumber, char cardValue, char cardSuit);
 
 ///Call this when playerNumber mucks his/her hand during the showdown.
 ///Note: If a player doesn't PlayerShowsCard() then a muck is assumed
-static PyObject * PlayerMucksHand (PyObject *self, PyObject *args) //void (int8 playerNumber);
-{
-}
+extern "C" void PlayerMucksHand(int8 playerNumber);
+
+
 
 
 
 ///Call this when new hands are dealt
-static PyObject * StartDealNewHands (PyObject *self, PyObject *args) //void ();
-{
-}
+extern "C" void StartDealNewHands();
 
 /*****************************************************************************
 	Event functions
@@ -213,47 +168,26 @@ static PyObject * StartDealNewHands (PyObject *self, PyObject *args) //void ();
 	BEGIN
 	Initial setup functions
 
-static PyObject * SetBigBlind (PyObject *self, PyObject *args) //Note: () and SetSmallBlind() can be called between
+Note: SetBigBlind() and SetSmallBlind() can be called between
 hands anytime the blind size changes during the game
 *****************************************************************************/
 
 ///Choose playerNumber to be the dealer for the first hand
-static PyObject * InitChooseDealer (PyObject *self, PyObject *args) //void (int8 playerNumber);
-{
-}
+extern "C" void InitChooseDealer(int8 playerNumber);
 
 ///Set the amount of money that the SMALLEST chip is worth
-static PyObject * InitSmallestChipSize (PyObject *self, PyObject *args) //void (float64 money);
-{
-}
+extern "C" void InitSmallestChipSize(float64 money);
 
 ///Call this when the big blind has changed
-static PyObject * SetBigBlind (PyObject *self, PyObject *args) //void ();
-{
-}
+extern "C" void SetBigBlind();
 
 ///Call this when the small blind has changed
-static PyObject * SetSmallBlind (PyObject *self, PyObject *args) //void ();
-{
-}
+extern "C" void SetSmallBlind();
 
 ///Add a player to the table. PLAYERS MUST BE ADDED IN CLOCKWISE ORDER.
 ///The function returns a playerNumber to identify this player in your code
-static PyObject * AddHumanOpponent (PyObject *self, PyObject *args) //int8 (string playerName, float64 initialMoney);
-{}
-
-static PyObject * AddStrategyBot_ConservativeDefence (PyObject *self, PyObject *args) //int8 (string playerName, float64 initialMoney);
-{}
-
-static PyObject * AddStrategyBot_GambleDefence (PyObject *self, PyObject *args) //int8 (string playerName, float64 initialMoney);
-{}
-
-static PyObject * AddStrategyBot_ConservativeOffence (PyObject *self, PyObject *args) //int8 (string playerName, float64 initialMoney);
-{}
-
-static PyObject * AddStrategyBot_GambleOffence (PyObject *self, PyObject *args) //int8 (string playerName, float64 initialMoney);
-{}
-
+extern "C" int8 AddHumanOpponent(string playerName);
+extern "C" int8 AddStrategyBot(string playerName, char botType);
 
 
 /*****************************************************************************
