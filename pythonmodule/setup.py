deb20709dd70a9e0f6c614ffa3d17b79916cf969
Joseph Huang <yuzisee@gmail.com>
2008-11-03 15:25:26 +0000

    ranlib archive for Unix works though makefile. DLL works in Windows.

--- pythonmodule/setup.py
+++ pythonmodule/setup.py
@@ -7,12 +7,6 @@ from distutils.core import setup, Extension
 import sys
 
 #Choose (getting MinGW and VCC to work together via DLLs, do we need this? http://www.mingw.org/phpwiki-1.3.14/index.php/MSVC-MinGW-DLL)
-linkstatically = False
-linkdynamically = True
-
-
-if (linkstatically and linkdynamically) or (not linkstatically and not linkdynamically):
-	raise Exception,  "Link dynamically or statically?? Choose only one please"
 
 packagename = 'Holdem'
 extensionname = '_holdem'
@@ -20,25 +14,21 @@ sourcefiles = ['holdemmodule.c']
 
 # What OS am I? http://docs.python.org/library/sys.html#sys.platform
 if sys.platform[:3] == 'win':
-    #But we'll use MinGW anyways.
-    if linkstatically:
-        raise Exception,  "Not sure where VC++ static objects are yet.\Try setting linkdynamically=True at the top of this file."
-    elif linkdynamically:
-        module1 = Extension(extensionname,
-#                    extra_objects = ['../holdem/holdemdll/Release/holdemDLL.dll'],  # MinGW is smart enough to figure this out
-                    extra_objects = ['../holdem/holdemdll/Release/holdemDLL.lib'],
-                    sources = sourcefiles)
+    #But we'll use MinGW anyways and link dynamically
+    module1 = Extension(extensionname,
+#                extra_objects = ['../holdem/holdemdll/Release/holdemDLL.dll'],  # MinGW is smart enough to figure this out
+                extra_objects = ['../holdem/holdemdll/Release/holdemDLL.lib'],
+                sources = sourcefiles)
 else:
 #Assume Posix
-    if linkstatically:
-        module1 = Extension(extensionname,
-                    extra_objects = ['../lib/holdem.a'],
-                    sources = sourcefiles)
-    elif linkdynamically:
-        module1 = Extension(extensionname,
-                    libraries = ['holdem.so'],
-                    library_dirs = ['../lib'],
-                    sources = sourcefiles)
+    module1 = Extension(extensionname,
+                extra_objects = ['libholdem.a'],
+                sources = sourcefiles)
+
+#    module1 = Extension(extensionname,
+#                libraries = ['libholdem.so'],
+#                library_dirs = ['./'],
+#                sources = sourcefiles)
 	
 #Possible options to Extension contstructor: http://docs.python.org/distutils/apiref.html?highlight=extension#distutils.core.Extension
 
