285f933ad90cf6a4170b21ab61bf719667968b6f
John Huang <j13huang@gmail.com>
2008-12-31 02:20:39 +0000

    Card images done

--- guipoker/src/CardImagesQt.py
+++ guipoker/src/CardImagesQt.py
@@ -1,6 +1,8 @@
 import Image
+import ImageQt
+from PyQt4.QtCore import Qt
 
-class CardImageDeck:
+class CardImageDeck(UserDict):
     _CARD_LENGTH = 123
     _CARD_WIDTH = 79
     _CARDS_IMAGE = Image.open('cards.png')
@@ -8,21 +10,18 @@ class CardImageDeck:
     _IMAGE_SUIT_SYMBOL = ['c','d','h','s']
 
     def __init__(self):
-        self.card_dict = {}
         for x in range(12):
             for y in range(4):
                 card_image_region = (x * _CARD_LENGTH, (x + 1) * _CARD_LENGTH, y * _CARD_LENGTH, (y + 1) * _CARD_LENGTH)
                 cropped_card_image = _CARDS_IMAGE.crop(card_image_region)
                 card_image = CardImage(cropped_card_image)
-                self.card_dict[self._card_symbol(x, y)] = card_image
-        self.card
-
-    def get_card_image(self, card_symbol):
-        return self.deck_dict[card_symbol]
-    
+                self[self._card_symbol(x, y)] = card_image
+        card_back_image_region = (2 * _CARD_LENGTH, 3 * _CARD_LENGTH, 4 * _CARD_LENGTH, 5 * _CARD_LENGTH)
+        self['back'] = CardImage(card_back_image_region)
+        
     def _card_symbol(self, rank, suit):
         return _IMAGE_RANK_SYMBOL[rank] + _IMAGE_SUIT_SYMBOL[suit]
 
-class CardImage:
+class CardImage(QPixmap):
     def __init__(self, card_image):
-        self.image = card_image
+        self.fromImage(ImageQt.ImageQt(card_image))
