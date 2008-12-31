705302ce771471c15a7f53b7d384dcb43c38245a
John Huang <j13huang@gmail.com>
2008-12-31 14:17:03 +0000

    Works when I do it.

--- guipoker/src/PokerGUI.py
+++ guipoker/src/PokerGUI.py
@@ -48,25 +48,31 @@ class DisplayManyLabelsTopRow(QHBoxLayout):
 class EllipseLayout(QLayout):
 	
 	def addItem(self, item):
+		if not isinstance(item,QLayoutItem):
+			raise TypeError, 'This exception is not object oriented, but Ruby is, so use addWidget'
 		if len(self._item_list) >= self._max_items:
 			print 'too many items'
 			return
 		self._item_list.append(item)
 	
 	def sizeHint(self):
-		return 34
+		return 1
 	
 	def setGeometry(self, q_rect):
+		if len(self._item_list) == 0:
+			return
 		num_items = len(self._item_list)
-		item_spacing = (2 / num_items) * math.pi #in radians
+		item_spacing = (2 * math.pi) / num_items  #in radians
 		for index in range(num_items):
-			self._item_list[index].setGeometry(QRect(62* math.sin(index * item_spacing), 34*math.cos(index * item_spacing), 34, 62))
+			mysize = QRect(100+100* math.sin(index * item_spacing), 100+100*math.cos(index * item_spacing), 100, 200);
+			self._item_list[index].setGeometry(mysize)
+			print repr(mysize.getCoords())
 		
 	def itemAt(self, index):
-		print self._item_list
-		print index
-		print 'hooooooo'
-		return self._item_list[0]#index]
+		if index < len(self._item_list):
+			return self._item_list[index]
+		else:
+			return None
 	
 	def count(self):
 		return len(self._item_list)
@@ -74,17 +80,19 @@ class EllipseLayout(QLayout):
 	def takeAt(self, index):
 		return self._item_list.pop(index)
 	#addItem(), sizeHint(), setGeometry(), itemAt() and takeAt(). You should also implement minimumSize() to e
+	
+	def expandingDirections(self):
+		return 0
 
 	def __init__(self, parent=None, max_items = 13):
-		super(EllipseLayout, self)
+		super(EllipseLayout, self).__init__(parent)
 		self._item_list = []
 		self._max_items = max_items
-		QGridLayout.__init__(self, parent)
 
 		#Center green
 
-		test_label = QLabel('QLabel')
-		test_label.setStyleSheet("QWidget { background-color: %s }" % QColor(0, 150, 50).name())
+		#test_label = QLabel('QLabel')
+		#test_label.setStyleSheet("QWidget { background-color: %s }" % QColor(0, 150, 50).name())
 		#self.addWidget(test_label) #Span (rows-2) rows and 1 column, starting at row 1, column 1
 
 
@@ -110,11 +118,19 @@ class QuitButtonWindow(QWidget):
 
 		#self.setLayout(DisplayManyLabelsLayout())
 		card_table = EllipseLayout()
-		card_table.addItem(QLabel('HOOOO'))#DisplayInfo(card_image_deck['back']))
+		hoo = QLabel('HOO',self)
+		hoo1 = QLabel('HOO1',self)
+		hoo2 = QLabel('HOO2',self)
+		hoo3 = QLabel('HOO3',self)
+		
 		self.setLayout(card_table)
+		card_table.addWidget(hoo)#DisplayInfo(card_image_deck['back']))
+		card_table.addWidget(hoo1)
+		card_table.addWidget(hoo2)
+		card_table.addWidget(hoo3)
 
 		quit_button = QPushButton('Close',self)
-		quit_button.setGeometry(2, 8, 60, 35)
+		quit_button.setGeometry(180, 8, 60, 35)
 
 
 		self.connect(quit_button, QtCore.SIGNAL('clicked()'),
