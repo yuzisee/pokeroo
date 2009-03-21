07acae97838b68248f1bf142443a8712e521c5e8
Joseph Huang <yuzisee@gmail.com>
2009-03-21 06:21:43 +0000

    Scrollable selectable histories

--- consoleseparate/consoleseparate.py
+++ consoleseparate/consoleseparate.py
@@ -26,10 +26,35 @@ class SubProcessThread(threading.Thread):
             time.sleep(0.5)   # read at most x times / sec
 
 
-#class WindowThread(threading.Thread):
+class HistoryText(Tkinter.Frame):
+
+    def __init__(self, parent, **options):
+        Tkinter.Frame.__init__(self,parent,options)
+        self.my_scrollbar = Tkinter.Scrollbar(self)
+
+        self.my_text = Tkinter.Text(self,height=0,yscrollcommand=self.my_scrollbar.set)
+        self.my_scrollbar.config(command=self.my_text.yview)
+
+        self.config(relief=self.my_text.cget("relief"))
+
+    def set_font(self,new_font):
+        self.my_text.configure(font=new_font)
+
+    def add_text(self,new_text):
+        self.my_text.configure(state=Tkinter.NORMAL)
+        self.my_text.insert(Tkinter.END,new_text)
+        self.my_text.configure(state=Tkinter.DISABLED)
+        self.my_text.see(Tkinter.END)
+
+    def setup_geometry_manager(self):
+        self.my_scrollbar.pack(side=Tkinter.RIGHT, fill=Tkinter.Y)
+        self.my_text.pack(fill=Tkinter.BOTH,expand=1)
 
 
 
+
+def user_input(event):
+    print "User pressed" + repr(event.char)
 
 
 if __name__=='__main__':
@@ -40,16 +65,28 @@ if __name__=='__main__':
     expand_all =Tkinter.W+Tkinter.E+Tkinter.N+Tkinter.S
     expand_bottom_horizontal =Tkinter.W+Tkinter.E+Tkinter.S
 
+    console_font = font=("Lucida Console", 9)
+
+
     #
     #The root window is composed of four quarters:
     #Left is stdout, Right is stderr
     #Top is history, Bottom is input
     #
 
-    stdout_history = Tkinter.Label(root_window, text="out top", borderwidth=2, relief=Tkinter.SUNKEN)
-    stderr_history = Tkinter.Label(root_window, text="err top", borderwidth=2, relief=Tkinter.SUNKEN)
+    stdout_history_frame = HistoryText(root_window)
+    stdout_history_frame.set_font(console_font)
+    stdout_history_frame.add_text("out t\no\np\n\n\n\n\nd")
 
-    stdout_latest  = Tkinter.Label(root_window, text="out bottom\n \ne\nr\nr bottom\nnbb\nf err bottom\nnbb\nf", borderwidth=2, relief=Tkinter.GROOVE)
+
+
+
+    stderr_history_frame = HistoryText(root_window)
+    stderr_history_frame.set_font(console_font)
+    stderr_history_frame.add_text("err top")
+
+
+    stdout_latest  = Tkinter.Label(root_window, text="out bottom\n \ne\nr\nr bottom\nnbb\nf err bottom\nnbb\nf", borderwidth=2, relief=Tkinter.GROOVE, font=console_font, justify=Tkinter.LEFT, anchor=Tkinter.W)
 
     #The input frame contains the stderr latest with an entry field at the bottom
     stderr_input_frame = Tkinter.Frame(root_window, borderwidth=2, relief=Tkinter.GROOVE)
@@ -58,18 +95,26 @@ if __name__=='__main__':
     stderr_input  = Tkinter.Entry(stderr_input_frame, relief=Tkinter.SUNKEN,width=0)
     stderr_input.pack(side=Tkinter.BOTTOM,fill=Tkinter.X,expand=0)
 
-    stderr_latest  = Tkinter.Label(stderr_input_frame, text="err bottom\nnbb\nf", relief=Tkinter.FLAT)
+    stderr_latest  = Tkinter.Label(stderr_input_frame, text="err bottom\nnbb\nf", relief=Tkinter.FLAT, font=console_font, justify=Tkinter.LEFT, anchor =Tkinter.W)
     stderr_latest.pack(side=Tkinter.TOP,fill=Tkinter.BOTH,expand=1)
 
     #Grid layout is resizable
-    stdout_history.grid(row=0,column=0,sticky=expand_all)
+    stdout_history_frame.setup_geometry_manager()
+    stdout_history_frame.grid(row=0,column=0,sticky=expand_all)
     stdout_latest.grid(row=1,column=0,sticky=expand_bottom_horizontal)
-    stderr_history.grid(row=0,column=1,sticky=expand_all)
+
+    stderr_history_frame.setup_geometry_manager()
+    stderr_history_frame.grid(row=0,column=1,sticky=expand_all)
     stderr_input_frame.grid(row=1,column=1,sticky=expand_bottom_horizontal)
 
     root_window.grid_columnconfigure(0,weight=1)
     root_window.grid_columnconfigure(1,weight=1)
     root_window.grid_rowconfigure(0,weight=1)
     root_window.grid_rowconfigure(1,weight=0)
+
+    #Event capture
+    stderr_input.bind("<Key>",user_input)
+
+    #Initialize view
 
     root_window.mainloop()
