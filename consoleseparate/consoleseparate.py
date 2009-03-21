4a841656cfe15d57caac5488a5c15e9380f8c14d
Joseph Huang <yuzisee@gmail.com>
2009-03-21 07:48:40 +0000

    Consoleseparate just about working

--- consoleseparate/consoleseparate.py
+++ consoleseparate/consoleseparate.py
@@ -11,6 +11,9 @@ import Tkinter
 #for Popen
 import subprocess
 
+#for os.read
+import os
+
 class SubProcessThread(threading.Thread):
     MAXIMUM_BYTE_READ = 2048
 
@@ -20,10 +23,11 @@ class SubProcessThread(threading.Thread):
         self._stopreading_test = returncode_test
         self._txt_callback = text_append_callback
 
-    def run():
+    def run(self):
         #From http://mail.python.org/pipermail/python-list/2007-June/618721.html
-        while self._stopreading_test():
-            self._txt_callback(os.read(self._fd, MAXIMUM_BYTE_READ))
+        while self._stopreading_test() is None:
+            print "performing read"
+            self._txt_callback(os.read(self._fd, SubProcessThread.MAXIMUM_BYTE_READ))
 
         #txt =
 #        time.sleep(0.5)   # read at most x times / sec
@@ -64,7 +68,7 @@ class UserEntry(Tkinter.Entry):
     def bind_input_output(self,app_stdin,input_output_pairs):
         self.label_history_pairs = input_output_pairs
         self.app_stdin = app_stdin
-        stderr_input.bind("<Return>",self.capture_return_event)
+        self.bind("<Return>",self.capture_return_event)
 
     def capture_return_event(self,event):
 #        print "User pressed" + repr(event.char)
@@ -77,7 +81,7 @@ class UserEntry(Tkinter.Entry):
 #            for lh_pair in self.label_history_pairs:
 #                lh_pair[0].configure(text=lh_pair[0].cget("text")+event.char+'\r\n')
         self.app_stdin.write(event.widget.get())
-        event.widget.delete(0, END)
+        event.widget.delete(0, Tkinter.END)
 
 
 class ConsoleSeparateWindow(Tkinter.Tk):
@@ -87,8 +91,8 @@ class ConsoleSeparateWindow(Tkinter.Tk):
 
     DEFAULT_CONSOLE_FONT = font=("Lucida Console", 9)
 
-    def __init__(self, my_console_app, **options):
-        Tkinter.Tk.__init__(self,options)
+    def __init__(self, my_console_app):
+        Tkinter.Tk.__init__(self)
 
         self._my_subprocess = my_console_app
         self._stdout_latest = None
@@ -104,32 +108,30 @@ class ConsoleSeparateWindow(Tkinter.Tk):
         #
 
         stdout_history_frame = HistoryText(self)
-        stdout_history_frame.set_font(DEFAULT_CONSOLE_FONT)
-        stdout_history_frame.add_text("out t\no\np\n\n\n\n\nd")
-        self._stdout_latest  = Tkinter.Label(self,borderwidth=2,relief=Tkinter.GROOVE, font=DEFAULT_CONSOLE_FONT, justify=Tkinter.LEFT, anchor=Tkinter.W)
+        stdout_history_frame.set_font(ConsoleSeparateWindow.DEFAULT_CONSOLE_FONT)
+        self._stdout_latest  = Tkinter.Label(self,borderwidth=2,relief=Tkinter.GROOVE, font=ConsoleSeparateWindow.DEFAULT_CONSOLE_FONT, justify=Tkinter.LEFT, anchor=Tkinter.W)
 
 
         stderr_history_frame = HistoryText(self)
-        stderr_history_frame.set_font(DEFAULT_CONSOLE_FONT)
-        stderr_history_frame.add_text("err top")
+        stderr_history_frame.set_font(ConsoleSeparateWindow.DEFAULT_CONSOLE_FONT)
 
         #The input frame contains the stderr latest with an entry field at the bottom
         stderr_input_frame = Tkinter.Frame(self, borderwidth=2, relief=Tkinter.GROOVE)
         stderr_input = UserEntry(stderr_input_frame,relief=Tkinter.SUNKEN,width=0)
 
-        self._stderr_latest  = Tkinter.Label(stderr_input_frame,relief=Tkinter.FLAT, font=DEFAULT_CONSOLE_FONT, justify=Tkinter.LEFT, anchor =Tkinter.W)
+        self._stderr_latest  = Tkinter.Label(stderr_input_frame,relief=Tkinter.FLAT, font=ConsoleSeparateWindow.DEFAULT_CONSOLE_FONT, justify=Tkinter.LEFT, anchor =Tkinter.W)
         self._stderr_latest.pack(side=Tkinter.TOP,fill=Tkinter.BOTH,expand=1)
 
         #Grid layout is resizable
         stderr_input.setup_geometry_manager()
 
         stdout_history_frame.setup_geometry_manager()
-        stdout_history_frame.grid(row=0,column=0,sticky=EXPAND_ALL)
-        self._stdout_latest.grid(row=1,column=0,sticky=EXPAND_BOTTOM_HORIZONTAL)
+        stdout_history_frame.grid(row=0,column=0,sticky=ConsoleSeparateWindow.EXPAND_ALL)
+        self._stdout_latest.grid(row=1,column=0,sticky=ConsoleSeparateWindow.EXPAND_BOTTOM_HORIZONTAL)
 
         stderr_history_frame.setup_geometry_manager()
-        stderr_history_frame.grid(row=0,column=1,sticky=EXPAND_ALL)
-        stderr_input_frame.grid(row=1,column=1,sticky=EXPAND_BOTTOM_HORIZONTAL)
+        stderr_history_frame.grid(row=0,column=1,sticky=ConsoleSeparateWindow.EXPAND_ALL)
+        stderr_input_frame.grid(row=1,column=1,sticky=ConsoleSeparateWindow.EXPAND_BOTTOM_HORIZONTAL)
 
         self.grid_columnconfigure(0,weight=1)
         self.grid_columnconfigure(1,weight=1)
@@ -137,7 +139,7 @@ class ConsoleSeparateWindow(Tkinter.Tk):
         self.grid_rowconfigure(1,weight=0)
 
         #Event capture
-        stderr_input.bind_input_output([(self._stdout_latest,stdout_history_frame),(self._stderr_latest,stderr_history_frame)])
+        stderr_input.bind_input_output(self._my_subprocess.stdin,[(self._stdout_latest,stdout_history_frame),(self._stderr_latest,stderr_history_frame)])
         self.protocol("WM_DELETE_WINDOW", self._destroy_handler)
 
     def _destroy_handler(self):
@@ -146,6 +148,7 @@ class ConsoleSeparateWindow(Tkinter.Tk):
 
     def append_stdout(self,append_text):
         self._stdout_latest.configure(text=self._stdout_latest.cget("text")+append_text)
+        print append_text,
 
     def append_stderr(self,append_text):
         self._stderr_latest.configure(text=self._stderr_latest.cget("text")+append_text)
@@ -156,7 +159,8 @@ if __name__=='__main__':
     #   Execute our child process
     #================================
     #From http://mail.python.org/pipermail/python-list/2007-June/618721.html
-    console_app = subprocess.Popen(sys.argv, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE,bufsize=1)
+    console_app = subprocess.Popen(sys.argv[1:], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE,bufsize=1)
+
 
     #===============================================
     #   Create the application window via Tkinter
@@ -167,8 +171,12 @@ if __name__=='__main__':
     #===========================================
     #   Bind stdout and stderr to root_window
     #===========================================
-    stdout_capturer = SubProcessThread(console_app.stdout, console_app.returncode, root_window.append_stdout)
-    stderr_capturer = SubProcessThread(console_app.stderr, console_app.returncode, root_window.append_stderr)
+    stdout_capturer = SubProcessThread(console_app.stdout, lambda: console_app.returncode, root_window.append_stdout)
+    stderr_capturer = SubProcessThread(console_app.stderr, lambda: console_app.returncode, root_window.append_stderr)
 
-    #Initialize view
+    #==========
+    #   Run!
+    #==========
+    stdout_capturer.start()
+    stderr_capturer.start()
     root_window.mainloop()
