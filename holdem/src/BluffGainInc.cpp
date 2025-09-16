yuzisee/pokeroo

commit 9f6bf7a97f63b0c1c23840d50653aec0a2c3bf33

Fix a crash in FindZero where one of the values is exactly zero.
Also add a script for generating regression tests
and improve some logging

holdem/src/functionbase.cpp
@@ -635,6 +635,9 @@ float64 ScalarFunctionModel::FindZero(float64 x1, float64 x2, bool bRoundToQuant
     float64 y1 = f(x1);
     float64 y2 = f(x2);
 
+    if (y1 == 0) return y1;
+    if (y2 == 0) return y2;
+
     if( y1 > 0 && y2 > 0 ) //x1*x2 > 0
     {
         if( y1 > y2 ) return x2;

holdem/appsrc/testDriver.cpp
@@ -490,7 +490,8 @@ static std::string testPlay(std::string gameId, char headsUp = 'G', std::ostream
     }else
     {
 //        smallBlindChoice=AUTO_CHIP_COUNT/200;
-        smallBlindChoice=AUTO_CHIP_COUNT/35;
+        smallBlindChoice=AUTO_CHIP_COUNT/50;
+//        smallBlindChoice=AUTO_CHIP_COUNT/35;
     }
     BlindValues b;
     b.SetSmallBigBlind(smallBlindChoice);
@@ -753,7 +754,8 @@ static void superGame(char headsUp = 0)
 
     
 
-    std::ofstream tourny("batchResults.txt", std::ios::trunc);
+    std::ofstream tourny("batchResults.txt", std::ios::app);
+    tourny << " - - - - - \n";
     tourny << iWin.c_str() << endl;
     tourny.close();
 

holdem/src/arenaFlow.cpp
@@ -383,7 +383,7 @@ void HoldemArena::PrepBettingRound(bool bFirstBettingRound, uint8 otherBettingRo
 
 int8 HoldemArena::PlayRound_BeginHand(std::ostream &gamelog)
 {
-    gamelog << "BEGIN" << endl;
+    gamelog << "BEGIN " << GetChipDenom() << endl;
 
 
     //Preflop

holdem/unittests/audit.py
@@ -1,0 +1,255 @@
+#!/usr/bin/env python
+"""A script for generating a regression test.
+
+Usage:
+  Run this script, paste in the gamelog for a hand, and press Ctrl+d
+
+  The only remaining edits you'll have to make:
+   + the bot type
+   + hand number
+   + bot's hole cards
+   + which player is the bot
+
+"""
+
+import math
+import re
+import sys
+
+
+PLAYER_NAME_CHIP_COUNT = re.compile('^\s*\[([^ ]+) \$([0-9.]+)\]')
+PLAYER_CALLS = re.compile('^(.*) calls \$([0-9.]+) \(\$[0-9.]+\)')
+PLAYER_RAISES = re.compile('^(.*) raises.*to \$([0-9.]+) \(\$[0-9.]+\)')
+
+
+
+#//  [0][1][2][3][4][5][6][7][8][9]
+#//0. 2S 2H 2C 2D 3S 3H 3C 3D 4S 4H
+#//1. 4C 4D 5S 5H 5C 5D 6S 6H 6C 6D
+#//2. 7S 7H 7C 7D 8S 8H 8C 8D 9S 9H
+#//3. 9C 9D TS TH TC TD JS JH JC JD
+#//4. QS QH QC QD KS KH KC KD AS AH
+#//5. AC AD
+
+VALUE_START = {'2': 0, '3': 4, '4': 8, '5': 12, '6': 16,
+               '7': 20, '8': 24, '9': 28, 'T': 32, 'J': 36,
+               'Q': 40, 'K': 44, 'A': 48}
+
+SUIT_PLUS = {'s': 0, 'h': 1, 'c': 2, 'd': 3}
+
+def cardnum(s):
+    return VALUE_START[s[0]] + SUIT_PLUS[s[1]]
+
+class Player(object):
+    def __init__(self, ident, chipcount):
+        """Create a player to track what bets it makes
+
+        Parameters:
+          ident:
+            (string) Player name
+          chipcount:
+            (float) Starting chip count
+
+        """
+       
+        self.bets = [];
+        self.ident = ident
+        self.chipcount = chipcount
+
+    def add_bet(self, amount):
+        """Store a bet made.
+
+        Parameters:
+          amount:
+            (float)
+            The bet "to" amount. If calling, you can also put NaN.
+
+        """
+        assert isinstance(amount, float)
+        self.bets.append(amount)
+
+    def print_as_code(self, idx):
+
+      print('')
+      if all(b == 0.0 for b in self.bets):
+        print('FixedReplayPlayerStrategy d{0}(foldOnly);'.format(idx))
+      else:
+
+        bet_strings = []
+        for b in self.bets:
+            if math.isnan(b):
+                bet_strings.append('std::numeric_limits<float64>::signaling_NaN()')
+            else:
+                bet_strings.append(str(b))
+
+        print('static const float64 a' + str(idx) + '[] = {')
+        print(',\n'.join(bet_strings))
+        print('};')
+        print('FixedReplayPlayerStrategy d{0}(VectorOf(a{0}));'.format(idx))
+      print('myTable.ManuallyAddPlayer("{0}", {1}, &d{2});'.format(self.ident, self.chipcount, idx))
+      print('')
+
+
+def write_table(small_blind, dealer):
+    print('const playernumber_t dealer = {0};'.format(dealer))
+    print('')
+    print('struct BlindValues b;')
+    print('b.SetSmallBigBlind({0});'.format(small_blind))
+    print('')
+    print('HoldemArena myTable(b.GetSmallBlind(), true, true);')
+    print('myTable.setSmallestChip({0});'.format(small_blind))
+    print('')
+    print('const std::vector<float64> foldOnly(1, 0.0);')
+    print('')
+
+
+if __name__ == "__main__":
+  sys.stderr.write('Press Ctrl+D to end')
+  initializing = True
+  players = {}
+  for line in sys.stdin:
+
+     if line.startswith('Preflop'):
+        # This is the beginning of the hand. Clear everything.
+        player_order = []
+        assert len(players) == 0
+        flop = None
+        turn = None
+        river = None
+        assert initializing
+     elif initializing:
+        # Read out the player names and chip counts so we can get started.
+        next_player_line = PLAYER_NAME_CHIP_COUNT.match(line)
+        if next_player_line:
+            p = Player(next_player_line.group(1), float(next_player_line.group(2)))
+            players[p.ident] = p
+            player_order.append(p)
+            print('// Add player   {0}   ${1}'.format(p.ident, p.chipcount))
+            p = None
+        else:
+            # Not a line describing a player.
+            # In that case, either we're before the first player listing, or we've finished all player listing.
+            # An easy way to check is to see whether we have any players at all yet...
+            if players:
+                # We already saw some players. Since this line isn't a player listing, we must be done!
+                dealer = len(players) - 1
+                initializing = False
+     elif line.startswith('Flop:'):
+        # This is the beginning of the flop
+        flop = line.split()[1:4]
+     elif line.startswith('Turn:'):
+        # This is the beginning of the turn
+        turn = line.split()[4]
+     elif line.startswith('River:'):
+        # This is the beginning of the turn
+        river = line.split()[5]
+     elif not initializing:
+        # Okay, we aren't initializing anymore.
+        # Read out player bets and store them.
+        if ' posts SB of $' in line:
+            small_blind = line.split('$')[1].split()[0]
+            continue
+        elif ' posts BB of $' in line:
+            continue
+        elif ' folds' in line:
+            b = line.split()
+            assert len(b) == 2
+            players[b[0]].add_bet(0.0)
+        elif ' checks' in line:
+            b = line.split()
+            assert len(b) == 2
+            players[b[0]].add_bet(float('nan'))
+        else:
+            b = PLAYER_CALLS.match(line)
+            if b:
+                players[b.group(1)].add_bet(float(b.group(2)))
+            b = PLAYER_RAISES.match(line)
+            if b:
+                players[b.group(1)].add_bet(float(b.group(2)))
+
+            
+
+  print('')
+  print('')
+  print('PureGainStrategy bot("0.txt", <#4#>);')
+  print('PlayerStrategy * const botToTest = &bot;')
+  print('')
+  write_table(small_blind, dealer)
+
+   
+
+  for n,p in enumerate(player_order):
+      p.print_as_code(n)
+   
+  print("""
+
+        DeckLocation card;
+
+        {
+// ==========================================================================
+            CommunityPlus handToTest; // .. ..
+
+            card.SetByIndex(<#card1#>);
+            handToTest.AddToHand(card);
+
+            card.SetByIndex(<#card2#>);
+            handToTest.AddToHand(card);
+
+            botToTest->StoreDealtHand(handToTest);
+        }
+
+
+        myTable.BeginInitialState(<#handnum#>);
+
+// ==========================================================================
+
+
+        myTable.BeginNewHands(std::cout, b, false, dealer);
+    
+""")
+
+
+  if flop is None:
+      print('myTable.PlayRound_BeginHand(std::cout)')
+  else:
+      print('assert(  myTable.PlayRound_BeginHand(std::cout)  !=  -1);')
+      print('// Flop: ' + ' '.join(flop))
+      print("""
+        CommunityPlus myFlop;
+
+        card.SetByIndex({0});
+        myFlop.AddToHand(card);
+
+        card.SetByIndex({1});
+        myFlop.AddToHand(card);
+
+        card.SetByIndex({2});
+        myFlop.AddToHand(card); 
+""".format(cardnum(flop[0]), cardnum(flop[1]), cardnum(flop[2])))
+      if turn is None:
+        print('myTable.PlayRound_Flop(myFlop, std::cout);')
+      else:
+        print('assert(myTable.PlayRound_Flop(myFlop, std::cout) != -1);')
+        print('// Turn: ' + turn)
+        print("""
+        DeckLocation myTurn;
+        myTurn.SetByIndex({0});
+        """.format(cardnum(turn)))
+        
+        if river is None:
+          print('myTable.PlayRound_Turn(myFlop, myTurn, std::cout);')
+        else:
+          print('assert(  myTable.PlayRound_Turn(myFlop, myTurn, std::cout)  !=  -1);')
+          print('// River: ' + river)
+          print("""
+        DeckLocation myRiver;
+        myRiver.SetByIndex({0});
+          """.format(cardnum(river)))
+          print('myTable.PlayRound_River(myFlop, myTurn, myRiver, std::cout);')
+          
+    
+           
+
+    
+        
+        

holdem/unittests/main.cpp
@@ -957,6 +957,122 @@ namespace RegressionTests {
     ;
 
 
+    void testRegression_028() {
+
+
+// -  stat_high algb SLIDERX -
+        // Qh Ac
+
+        // Add player   P2   $84.0
+        // Add player   Multi   $100.0
+        // Add player   P4   $210.0
+        // Add player   D2   $60.0
+        // Add player   Gear   $158.0
+        // Add player   P0   $84.0
+        // Add player   P3   $204.0
+        struct BlindValues b;
+        b.SetSmallBigBlind(2);
+
+        HoldemArena myTable(b.GetSmallBlind(), true, true);
+        myTable.setSmallestChip(2);
+
+        const playernumber_t dealer = 6;
+
+        const std::vector<float64> foldOnly(1, 0.0);
+        
+        PureGainStrategy bot("28.txt", 4);
+        PlayerStrategy * const botToTest = &bot;
+
+
+
+
+        FixedReplayPlayerStrategy d0(foldOnly);
+        myTable.ManuallyAddPlayer("P2", 84.0, &d0);
+
+
+        FixedReplayPlayerStrategy d1(foldOnly);
+        myTable.ManuallyAddPlayer("Multi", 100.0, &d1);
+
+
+        myTable.ManuallyAddPlayer("P4", 210.0, botToTest);
+
+
+        FixedReplayPlayerStrategy d3(foldOnly);
+        myTable.ManuallyAddPlayer("D2", 60.0, &d3);
+
+
+        static const float64 a4[] = {
+            158
+        };
+        FixedReplayPlayerStrategy d4(VectorOf(a4));
+        myTable.ManuallyAddPlayer("Gear", 158.0, &d4);
+
+
+        FixedReplayPlayerStrategy d5(foldOnly);
+        myTable.ManuallyAddPlayer("P0", 84.0, &d5);
+
+        
+        static const float64 a6[] = {
+            std::numeric_limits<float64>::signaling_NaN(),
+            std::numeric_limits<float64>::signaling_NaN(),
+            std::numeric_limits<float64>::signaling_NaN()
+        };
+        FixedReplayPlayerStrategy d6(VectorOf(a6));
+        myTable.ManuallyAddPlayer("P3", 204.0, &d6);
+        
+
+
+
+
+        DeckLocation card;
+
+        {
+            CommunityPlus handToTest; // Ah Qc
+
+            card.SetByIndex(41);
+            handToTest.AddToHand(card);
+
+            card.SetByIndex(50);
+            handToTest.AddToHand(card);
+
+            botToTest->StoreDealtHand(handToTest);
+        }
+
+
+        myTable.BeginInitialState(28);
+        myTable.BeginNewHands(std::cout, b, false, dealer);
+
+
+        assert(  myTable.PlayRound_BeginHand(std::cout)  !=  -1);
+               // Flop: 3d 5d 9s
+
+               CommunityPlus myFlop;
+
+               card.SetByIndex(7);
+               myFlop.AddToHand(card);
+
+               card.SetByIndex(15);
+               myFlop.AddToHand(card);
+
+               card.SetByIndex(28);
+               myFlop.AddToHand(card);
+
+               assert(myTable.PlayRound_Flop(myFlop, std::cout) != -1);
+               // Turn: Td
+
+               DeckLocation myTurn;
+               myTurn.SetByIndex(35);
+               
+               assert(  myTable.PlayRound_Turn(myFlop, myTurn, std::cout)  !=  -1);
+               // River: 6s
+               
+               DeckLocation myRiver;
+               myRiver.SetByIndex(16);
+               
+        myTable.PlayRound_River(myFlop, myTurn, myRiver, std::cout);
+
+
+    }
 
     // We hit the "You can't lose more than all your money: GainModelNoRisk" assertion
     void testRegression_027() {
@@ -4576,6 +4692,7 @@ int main(int argc, const char * argv[])
     
     // Regression tests
 
+    RegressionTests::testRegression_028();
     RegressionTests::testRegression_027();
     RegressionTests::testRegression_026();
     RegressionTests::testRegression_025();

commit bfaac4e7f680d797dadaa4751d5edd8ad4789b3d

Bugfix in ScalarFunctionModel::FindZero with additional assertions added to FoldOrCall::predictedRaiseToThisRound
First discovered by CodeRabbit during https://github.com/yuzisee/pokeroo/pull/50#pullrequestreview-3227582481

Bug was originally introduced by https://github.com/yuzisee/pokeroo/commit/9f6bf7a97f63b0c1c23840d50653aec0a2c3bf33

.gitignore
@@ -1,1 +1,7 @@
 holdem/objs/
+holdem/bin/
+holdem/lib/
+
+tags
+
+**/.DS_Store

holdem/src/BluffGainInc.cpp
@@ -28,6 +28,10 @@
 
 void AutoScalingFunction::query(float64 sliderx, float64 x)
 {
+float64 yl;
+float64 yr;
+float64 fd_yl;
+float64 fd_yr;
 
     last_x = x;
     last_sliderx = sliderx;
@@ -59,8 +63,10 @@ void AutoScalingFunction::query(float64 sliderx, float64 x)
 
         if( slider >= 1 )
         {
-            y = right.f(x);
-            dy = right.fd(x, yr);
+            yr = right.f(x);
+            fd_yr = right.fd(x, yr);
+
+            y = yr; dy = fd_yr;
 
 #ifdef DEBUG_TRACE_SEARCH
             if(bTraceEnable) std::cout << "\t\t\tbMax" << std::flush;
@@ -69,7 +75,7 @@ void AutoScalingFunction::query(float64 sliderx, float64 x)
         else if( slider <= 0 )
         {
             yl = left.f(x);
-            fd_yl = left.fd(x,yl);
+            fd_yl = left.fd(x, yl);
 
             y = yl; dy = fd_yl;
 

holdem/src/BluffGainInc.h
@@ -59,11 +59,6 @@ protected:
     float64 y;
     float64 dy;
 
-    float64 yl;
-    float64 yr;
-    float64 fd_yl;
-    float64 fd_yr;
-
 public:
 
 #ifdef TRANSFORMED_AUTOSCALES
@@ -249,7 +244,7 @@ public:
     firstFoldToRaise(-1)
     {
     }
-    
+
     /*
      StateModel(ExactCallBluffD &c, IFunctionDifferentiable &functionL, IFunctionDifferentiable &functionR) : ScalarFunctionModel(c.tableinfo->chipDenom()),HoldemFunctionModel(c.tableinfo->chipDenom(),c.tableinfo)
      ,last_x(-1)
@@ -310,4 +305,3 @@ public:
 
 
 #endif
-

holdem/src/callSituation.cpp
@@ -312,8 +312,10 @@ MeanOrRank FoldOrCall::suggestMeanOrRank() const {
     {}
 
 
+    // [!TIP]
+    // This is just a simple sum of a finite geometric series.
     float64 RaiseRatio::f(const float64 r)  {
-        float64 expected = fS;
+        float64 expected = fS; // We would like to reach this target amount at the end
         float64 actual = 0.0;
         float64 next = fA;
         for (int i=1;i<=fK;++i) {
@@ -337,7 +339,10 @@ MeanOrRank FoldOrCall::suggestMeanOrRank() const {
 
 float64 RaiseRatio::FindRatio() {
 
-    const float64 reraisedByFinalRatio = fS / fA; // If fK == 1 this would be the solution. If fK < 1 the required ratio only gets lower.
+    const float64 reraisedByFinalRatio = fS / fA;
+    // If it's the final betting round this would be the solution.
+    //         ^^^ i.e. `fK == 1` ^^^
+    // If fK > 1 the required ratio only gets lower.
 
     return FindZero(1.0, reraisedByFinalRatio, false);
 }
@@ -377,6 +382,15 @@ float64 FoldOrCall::predictedRaiseToThisRound(float64 actualBetToCall, float64 h
     if(is_nan(r)) {
         std::cout << "raiseRatio(" << quantum << "," << myRaiseBy << "," << reraisedByFinal << "," << static_cast<int>(spreadRaisesOverThisManyBettingRounds) << ") NaN'ed" << std::endl;
         exit(1);
+    }
+    if( (r < 1.0) && (start < reraisedByFinal)) {
+      std::cout << "We are outside the range? Raising would never cause the pot to SHRINK but... raiseRatio.FindRatio() returned " << r << " during FoldOrCall::predictedRaiseToThisRound" << std::endl;
+      exit(1);
+    }
+    if ((reraisedByFinal + std::numeric_limits<double>::epsilon()) / (start - std::numeric_limits<double>::epsilon()) < r - std::numeric_limits<double>::epsilon()) {
+      std::cout << "We want to answer the question of \"if their *target* raise is to reach reraisedByFinal: knowing there are `spreadRaisesOverThisManyBettingRounds` remaining, how much will they raise _this round_?\"  And, yet, raiseRatio.FindRatio() returned " << r
+      << " which would exceed their target of " << reraisedByFinal << " if the first raise is " << start << std::endl;
+      exit(1);
     }
 #endif // DEBUGASSERT
 

holdem/src/functionbase.cpp
@@ -671,8 +671,12 @@ float64 ScalarFunctionModel::FindZero(float64 x1, float64 x2, bool bRoundToQuant
     float64 y2 = f(x2);
 
     // See unittests/main.cpp#testRegression_028
-    if (y1 == 0) return y1;
-    if (y2 == 0) return y2;
+    if (y1 == 0) {
+      return x1;
+    }
+    if (y2 == 0) {
+      return x2;
+    }
 
     if( y1 > 0 && y2 > 0 ) //x1*x2 > 0
     {