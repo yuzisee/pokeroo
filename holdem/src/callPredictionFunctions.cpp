0b8462c5b07f89d069cf0b07e3e8e4d443142242
Joseph Huang <j25huang@uwaterloo.ca>
2025-09-22 23:57:59 -0700

    Logging for the various cases of https://github.com/yuzisee/pokeroo/pull/83

--- holdem/src/callPredictionFunctions.cpp
+++ holdem/src/callPredictionFunctions.cpp
@@ -278,7 +278,7 @@ template<typename T1, typename T2> float64 FoldWaitLengthModel<T1, T2>::rarity( 
 template<typename T1, typename T2> float64 FoldWaitLengthModel<T1, T2>::lookup( const float64 rank ) const
 {
     if( meanConv == 0 ) return rank;
-    return meanConv->nearest_winPCT_given_rank(rank);
+    return meanConv->nearest_winPCT_given_rank(rank).first;
 }
 
 template<typename T1, typename T2> float64 FoldWaitLengthModel<T1, T2>::dlookup( const float64 rank, const float64 lookupped ) const
@@ -331,7 +331,19 @@ template<typename T1, typename T2> float64 FoldWaitLengthModel<T1, T2>::f( const
 #endif // DEBUGASSERT
 
   #ifdef DEBUG_TRACE_PWIN
-			if(traceEnable != nullptr) *traceEnable << "\t\t\t\t FoldWaitLengthModel(n=" << n << ", bSearching=" << cached_d_dbetSize.b_assume_w_is_constant << ") compares remainingbet=" << remainingbet << " vs. betSize=" << betSize << " ↦ " << lastF << " based on (winShowdown)" << winShowdown << " ⋅ " << PW << "(PW)" << std::endl;
+			if(traceEnable != nullptr) {
+			  *traceEnable << "\t\t\t\t FoldWaitLengthModel(n=" << n << ", bSearching=" << cached_d_dbetSize.b_assume_w_is_constant << ") compares remainingbet=" << remainingbet << " vs. betSize=" << betSize << " ↦ " << lastF << " based on (winShowdown)" << winShowdown << " ⋅ " << PW << "(PW) vs. expected PW=";
+					const float64 reproduce_rawPct = getRawPCT(n);
+				*traceEnable << compute_dE_dbetSize(n, reproduce_rawPct, opponents);
+				if (meanConv == nullptr) {
+			     *traceEnable << " from " << reproduce_rawPct << "^" << opponents << std::endl;
+				} else {
+				  const std::pair<ValueAndSlope, char> reproduce_one_minus_rarity = meanConv->Pr_haveWorsePCT_continuous(w);
+				  const float64 reproduce_rarity = 1.0 - reproduce_one_minus_rarity.first.v;
+				  const std::pair<float64, char> reproduce_pct = meanConv->nearest_winPCT_given_rank(1.0 - 1.0 / n / reproduce_rarity);
+          *traceEnable << " on lookup 1:" << rarity() << " ∈ meanConv->Pr_haveWorsePCT_continuous(w) = " << reproduce_one_minus_rarity.first.v << "[" << reproduce_one_minus_rarity.second << "]  ▶  [" << reproduce_pct.second << "]" << reproduce_pct.first << " = " << reproduce_rawPct << "^" << opponents << std::endl;
+				}
+			}
 	#endif
 
     return lastF;
