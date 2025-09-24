5cbb663ef6ba0dda575a438755d3d1c6ac814ac8
yuzisee <yuzisee@users.noreply.github.com>
2025-09-24 09:11:34 +0000

    So will this work better?
    Review response to https://github.com/yuzisee/pokeroo/pull/91#discussion_r2374654274

--- holdem/appsrc/stratManual.cpp
+++ holdem/appsrc/stratManual.cpp
@@ -149,11 +149,11 @@ float64 ConsoleStepStrategy::MakeBet()
 
 		if (std::cin.peek() == '\n') { std::cin.get(); }
 		std::cout << "Press [Enter] to continue…" << std::flush;
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-    while (true) {
-      if (std::cin.get() == 13) { break; }
-    }
-    std::cin.sync();
+    	while (true) {
+      		if (std::cin.get() == 13) { break; }
+    	}
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    	std::cin.sync();
 	}
 	return ConsoleStrategy::MakeBet();
 }
