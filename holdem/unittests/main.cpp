540b59d14793ea5546df067c45d8c281398b3a7b
Joseph Huang <j25huang@uwaterloo.ca>
2025-09-24 07:33:11 -0700

    Prepare for https://github.com/yuzisee/pokeroo/pull/91 + https://github.com/yuzisee/pokeroo/pull/44 + https://github.com/yuzisee/pokeroo/pull/89

--- holdem/unittests/main.cpp
+++ holdem/unittests/main.cpp
@@ -4850,6 +4850,63 @@ Playing as S
         cps.alreadyBet,
         true
       };
+
+      // assert(dRiskLoss_pot >= 1.0 / (tablestate_tableinfo.handsIn()-1));
+      // [!CAUTION]
+      // (a) I haven't found a way to trigger `dRiskLoss_pot > 0.0` yet.
+      // (b) It's deprecated! From what I can tell
+      //       https://github.com/yuzisee/pokeroo/blob/0f04f077723249c7f141eed65efde732d1722f00/holdem/src/callSituation.cpp#L245
+      //     has deprecated `ExpectedCallD::RiskLoss` anyway and the only remaining callers
+      //      → ExactCallD::facedOdds_raise_Geom
+      //      → ExactCallD::dfacedOdds_dpot_GeomDEXF
+      //     ...should be switched over to OpponentHandOpportunity via CombinedStatResultsPessemistic
+      {
+        const ValueAndSlope actual_RiskLoss = tablestate_tableinfo.RiskLoss(hypothetical, (&core.callcumu));
+        assert((actual_RiskLoss.v == 0) && "Betting only 50.0 should be fine. No RiskLoss needed to discourage that?");
+      }
+
+      const float64 p4_raiseTo = 2400.0;
+      // const float mydexf = 1.0; // tablestate_tableinfo.RiskLoss(cps.alreadyBet, cps.bankroll, opponents, raiseto, useMean, &dRiskLoss_pot);
+      std::vector<std::pair<float64, ValueAndSlope>> actual_noRaisePct_vs_betSize;
+      // Mimic src/callPrediction.cpp#ExactCallD::dfacedOdds_dpot_GeomDEXF
+      // for (float64 betSize = 2000.0; betSize < 4001.0; betSize += 100.0) {
+      for (float64 p3_betSize = 250.0; p3_betSize < 2501.0; p3_betSize += 250.0) {
+        hypothetical.hypotheticalRaiseTo = p4_raiseTo;
+        hypothetical.hypotheticalRaiseAgainst = p3_betSize;
+
+        // To get a high P4 RiskLoss against P3, we want:
+        //  [FoldWaitLengthModel::FindBestLength]
+        //  → a high maxProfit, which means a high rawPCT (and/or low opponents)
+        //  → a high betSize
+        //  → a small amountSacrificePerHand, which means...
+        //    ... a large numHandsPerSameSituationFold, which means a very rare `rarity()`, which means
+        //      [ExpectedCallD::RiskLoss]
+        //      → a large N, which means a large `handsDealt()`
+        //    ... a small amountSacrificeVoluntary and small amountSacrificeForced, which means
+        //      [ExpectedCallD::RiskLoss]
+        //      → a small `avgBlind`
+        //      → a small ACTIVE pot (current round, players who haven't yet folded)
+        //      → a large rpAlreadyBet by P3
+        //      (and/or high player count)
+        // This RiskLoss heuristic reports a loss (negative value) if your bet is large enough for the average opponent to prot (opportunity) by folding and waiting for a better hand
+
+        const ValueAndSlope actual_RiskLoss = tablestate_tableinfo.RiskLoss(hypothetical, (&core.callcumu));
+        assert((actual_RiskLoss.v < 0) && "Raising from p3_betSize → hypothetical.hypotheticalRaiseTo is extreme on a table with 5 players. RiskLoss should be discouraging that.");
+
+        FacedOddsRaiseGeom<void> actual(myTable.GetChipDenom());
+        FacedOddsRaiseGeom<void>::configure_with(actual, hypothetical, actual_RiskLoss.v);
+        actual.FG.waitLength.load(cps, avgBlind);
+        actual.FG.waitLength.opponents = tablestate_tableinfo.handsToShowdownAgainst();
+        actual.FG.waitLength.setMeanConv(nullptr);
+        const float64 noRaisePct = actual.FindZero(0.0, 1.0, false);
+        const float64 d_noRaisePct_dbetsize = ExactCallD::dfacedOdds_raise_dfacedBet_GeomDEXF(tablestate_tableinfo, hypothetical, noRaisePct, actual_RiskLoss.D_v);
+
+        actual_noRaisePct_vs_betSize.push_back( std::pair<float64, ValueAndSlope>( p3_betSize , ValueAndSlope {
+          noRaisePct, d_noRaisePct_dbetsize
+        }));
+      }
+
+      assert(print_x_y_dy_derivative_ok(actual_noRaisePct_vs_betSize, 0.00001) > 0.75);
     } // end testHybrid_drisk_handsIn
 }
 
